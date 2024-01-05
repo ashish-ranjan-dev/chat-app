a8f5ad5057b475a46e407722287f0b3a7ba3adcd
Ashish Ranjan <ashish.ranjan@edifecs.com>
2024-01-05 16:25:32 +0530

    added chat data persistance and room chat feature

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const { Server } = require("socket.io")
 const { PORT } = require("./config/server-config.js");
 const connect = require('./config/db-config.js');
 
+const Group = require('./models/group');
+const Chat = require('./models/chat');
+
 const app = express();
 const server = http.createServer(app);
 
@@ -21,8 +24,13 @@ io.on("connection", (socket) => {
         socket.join(data.roomid);
     });
 
-    socket.on("new_msg", (data) => {
+    socket.on("new_msg", async (data) => {
         console.log("Received new message", data);
+        const chat = await Chat.create({
+            roomid: data.roomid,
+            sender: data.sender,
+            content: data.message
+        });
         // will emit message to particular roomid
         io.to(data.roomid).emit("msg_rcvd", data);
         // socket.emit("msg_rcvd", data);   // to self only
@@ -33,12 +41,34 @@ io.on("connection", (socket) => {
 
 app.set("view engine", "ejs");
 app.use("/", express.static(__dirname + "/public"));
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 app.get("/chat/:roomid/:user", async (req, res) => {
+    const group = await Group.findById(req.params.roomid);
+    const chats = await Chat.find({
+        roomid: req.params.roomid
+    });
+    // console.log(chats)
     res.render("index", {
         roomid: req.params.roomid,
         user: req.params.user,
+        groupname: group.name,
+        previousmsgs: chats
     });
+});
+
+app.get('/group', async (req, res) => {
+    res.render('group');
+});
+
+app.post('/group', async (req, res) => {
+    console.log(req.body);
+    await Group.create({
+        name: req.body.name
+    });
+    res.redirect('/group');
+
 });
 
 server.listen(PORT, async () => {
